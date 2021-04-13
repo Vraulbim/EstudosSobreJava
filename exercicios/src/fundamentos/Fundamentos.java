7ebf30d8e23ba4b459c903bed380ed3263b3fd38
Vitor Raul Bim <vitorrb36@gmail.com>
2021-04-13 17:00:03 -0300

    Estudo sobre tipos primitivos parte 1

--- exercicios/src/fundamentos/Fundamentos.java
+++ exercicios/src/fundamentos/Fundamentos.java
@@ -3,6 +3,8 @@ package fundamentos;
 public class Fundamentos {
 
     public static void main(String[] args) {
+
+        //var aceita apenas variaveis já definidas, e também não é permitido misturar os tipo de dados
         var a = 4.5;
 
         var b = "Texto";
---
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args){
+
+        //Valor representa a valor máximo das variaveis
+        byte anosDeEmpresa = 127;
+        short quantidadeDeVoos = 32767;
+        int id = 25478586;
+        //Se não colocarmos L no final de um long, o valor será salvo como inteiro
+        long pontosAcumulados =  3_134_845_223L;
+
+        float salario = 11_445.44f;
+    }
+}

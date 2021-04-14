ca97b3d442df53aa92d85b2f6d93dfc8bebc970d
Vitor Raul Bim <vitorrb36@gmail.com>
2021-04-14 14:03:18 -0300

    Parte 2

--- exercicios/src/fundamentos/TiposPrimitivos.java
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -11,6 +11,19 @@ public class TiposPrimitivos {
         //Se não colocarmos L no final de um long, o valor será salvo como inteiro
         long pontosAcumulados =  3_134_845_223L;
 
+        //Para o float temos que indicar que o valor é do tipo float finalizando o número com f
         float salario = 11_445.44f;
+        double vendasAcumuladas = 2_991_797_103.01;
+
+        boolean estaDeFerias = false;
+
+        char ativo = 'A';
+
+        System.out.println(anosDeEmpresa * 365);
+        System.out.println(quantidadeDeVoos / 2);
+        System.out.println(pontosAcumulados / vendasAcumuladas);
+        System.out.println(id + " ganha -> " + salario);
+        System.out.println("Ferias? " + estaDeFerias);
+        System.out.println("Ativo (A) ou Inativo (I) : " + ativo);
     }
 }
